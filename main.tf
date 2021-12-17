880c49f785c6f93099f7cced559b288f1c27f479
Serge Bogatyrev <sergebogatyrjov@gmail.com>
2021-12-18 02:57:05 +0300

    Changed vpc name

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "google" {
   region  = var.region
 }
 
-resource "google_compute_network" "xy-network" {
+resource "google_compute_network" "xy-network-1" {
   name                    = "${var.prefix}-vpc-${var.region}"
   auto_create_subnetworks = false
 }
