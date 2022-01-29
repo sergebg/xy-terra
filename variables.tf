608da7ecf783dd01cd2d01e8f040b7678250e2e2
Serge Bogatyrev <sergebogatyrjov@gmail.com>
2022-01-29 03:33:06 +0300

    Adding vars

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "project" {
+  type = string
+  default = "xy-com-332507"
+}
+
+variable "region" {
+  type = string
+  default = "europe-west3-c"
+}
+
+variable "prefix" {
+  type = string
+  default = "xy"
+}
