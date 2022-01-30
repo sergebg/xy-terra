3e4d1583b43a2c78220d8a8c366bf887f4c3f709
Serge Bogatyrev <sergebogatyrjov@gmail.com>
2022-01-30 21:13:14 +0300

    Adding subnet

--- main.tf
+++ main.tf
@@ -1,11 +1,16 @@
 provider "google" {
-  #version = "~> 2.0"
   project = var.project
   region  = var.region
 }
 
-resource "google_compute_network" "xy-network-1" {
+resource "google_compute_network" "net1" {
   name                    = "${var.prefix}-vpc-${var.region}"
   auto_create_subnetworks = false
 }
 
+resource "google_compute_subnetwork" "subnet1" {
+  name = ${var.prefix}-subnetwork-${var.region}
+  region = var.region
+  network = google_compute_network.net1.self_link
+  ip_cidr_range = "10.0.10.0/24"
+}
